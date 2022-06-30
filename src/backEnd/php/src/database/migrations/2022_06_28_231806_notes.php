4ed2ae9e72c8373557cf17880fa270ec51dcd2d9
Edward Aaron Russell <erussell9987@gmail.com>
2022-06-30 01:21:39 -0400

    database design

--- src/backEnd/php/src/database/migrations/2022_06_28_231806_notes.php
+++ src/backEnd/php/src/database/migrations/2022_06_28_231806_notes.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        //A Note has a title, description, creation date. A Note can also have multiple Tags.
+        Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('create_at');
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('notes');
     }
 };
