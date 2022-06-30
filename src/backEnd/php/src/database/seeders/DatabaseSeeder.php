f1e2961dfb91dfe642ef16c170750ff9d3c2a337
Edward Aaron Russell <erussell9987@gmail.com>
2022-06-30 01:22:14 -0400

    some basic test data

--- src/backEnd/php/src/database/seeders/DatabaseSeeder.php
+++ src/backEnd/php/src/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Services\NoteService;
+use App\Services\TagService;
+use DateTime;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,13 +15,18 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run(NoteService $noteService, TagService $tagService){
+        $tagService->create("tag 1");
+        $tagService->create("tag 2");
+        $tagService->create("tag 3");
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $tags = $tagService->getAll();
+
+//        dd($tags);
+
+        $noteService->create("Title 1", "Note Description 1", new DateTime(), $tags->random(2)->toArray());
+        $noteService->create("Title 2", "Note Description 2", new DateTime(), $tags->random(2)->toArray());
+        $noteService->create("Title 3", "Note Description 3", new DateTime(), $tags->random(2)->toArray());
+
     }
 }
