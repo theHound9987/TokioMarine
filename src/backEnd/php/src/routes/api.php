44563f018755e29f3b5676795a908b3bf07e1f57
Edward Aaron Russell <erussell9987@gmail.com>
2022-06-30 01:27:17 -0400

    add all routes needed for the system

--- src/backEnd/php/src/routes/api.php
+++ src/backEnd/php/src/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostTagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GetNotesController;
+use App\Http\Controllers\GetTagsController;
+use App\Http\Controllers\PostNoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/notes', GetNotesController::class);
+Route::post('/notes', PostNoteController::class);
+Route::get('/tags', GetTagsController::class);
+Route::post('/tags', PostTagController::class);
